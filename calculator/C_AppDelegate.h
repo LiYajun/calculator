d7b6cd946c284b5ce593a244cc5f8bcff86672ac
andybain <macmini04@thumbsoft-andybain.local>
2014-05-19 10:29:57 +0800

    Initial Commit

---
+++ calculator/C_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  C_AppDelegate.h
+//  calculator
+//
+//  Created by andybain on 14-5-19.
+//  Copyright (c) 2014年 thumbsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface C_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calculator
+//
+//  Created by andybain on 14-5-19.
+//  Copyright (c) 2014年 thumbsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "C_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([C_AppDelegate class]));
+    }
+}
